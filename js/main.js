e0d01f893325490a9019e1d5ac80ae3ec5007fb8
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-05-29 16:56:32 +0200

    Completed the E-commerce page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+//! The following code is implemented to manage the galerry
 let thumbnail = document.querySelectorAll(".product-thumbnail");
 let mainImg = document.getElementById("mainProductImg");
-
-//? the following logic is to chnage the main cover img once thumbnail selected:
+// the following logic is to chnage the main cover img once thumbnail selected:
 thumbnail.forEach((element, index) => {
     element.addEventListener('click', ()=> {
         mainImg.src = `images/image-product-${index+1}.jpg`;
@@ -9,7 +9,7 @@ thumbnail.forEach((element, index) => {
         element.classList.add("active");
     });
 });
-//? The following function is occupiad to open the lightbox
+// The following function is occupiad to open the lightbox
 mainImg.addEventListener('click',()=>openLightbox());
 function openLightbox () {
     document.getElementById('lightbox-img').src = mainImg.src;
@@ -33,9 +33,22 @@ function prevSlide () {
     document.getElementById('lightbox-img').src = `images/image-product-${index}.jpg`;
 }
 // TODO: The following function is occupiad to the cart logic
-cartItems = 0;
-function addToCart () {
+let cartIcon = document.getElementById("cartIcon");
+let cartPopUp = document.getElementById("cartPopup");
+let addToCartBtn = document.getElementById("addToCart");
+let cartItemCount = document.getElementById("cart-count");
 
+cartIcon.onmouseenter = ()=> {cartPopUp.style.display = 'flex';}
+cartIcon.onmouseleave = ()=> {cartPopUp.style.display = 'none';}
+
+addToCartBtn.addEventListener('click',  addToCart )
+cartItems = 0;
+
+function addToCart () {
+    console.log('add to cart called');
+    console.log(Number.parseInt(cartItemCount.textContent ++ ));
+    cartItemsCount = Number.parseInt(cartItemCount.textContent ++);
+    cartItemCount.textContent = cartItemsCount;
 }
 function toggleCart (){
 
@@ -61,5 +74,4 @@ decrementBtn.addEventListener('click', ()=> {
     count = count -1;
     itemCount.textContent = count;
     }
-
 });
